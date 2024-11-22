423b054ed1def1cd401e8aab7a1710e07fe1f6bb
alfiefernandes <alfiefernandes2@gmail.com>
2024-11-22 17:23:23 +0700

    fix bancard to large

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -14,7 +14,7 @@ const BankCard = ({
     <div className="flex flex-col">
       <Link
         href={`/transaction-history/?id=${account.appwriteItemId}`}
-        className="bank-card min-w-[325px]"
+        className="bank-card min-w-[290px]"
       >
         <div className="bank-card_content">
           <div>
